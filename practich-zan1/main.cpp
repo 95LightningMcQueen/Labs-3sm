532cba5e59f189a64526fe58d18d942a27ce13b5
Молния Маккуин <itismytwink32@gmail.com>
2025-10-24 18:19:33 +0300

    Update main.cpp

--- practich-zan1/main.cpp
+++ practich-zan1/main.cpp
@@ -18,7 +18,7 @@ class Fraction
 {
 private:
     uint64_t num;
-    uint64_t den; 
+    uint64_t den;
     bool neg;
     void normalizeFraction()
     {
@@ -84,11 +84,11 @@ public:
             result_neg = false;
         }
         uint64_t result_num;
-        if (sum < 0) 
+        if (sum < 0)
         {
             result_num = -sum;
         }
-        else 
+        else
         {
             result_num = sum;
         }
@@ -144,7 +144,7 @@ public:
     }
 
 
-    Fraction operator*(const Fraction& rhs) const
+    Fraction operator*(const Fraction& rhs)
     {
         Fraction result;
         result.neg = neg ^ rhs.neg;
@@ -154,7 +154,7 @@ public:
         return result;
     }
 
-    Fraction operator/(const Fraction& rhs) const
+    Fraction operator/(const Fraction& rhs)
     {
         if (rhs.num == 0)
         {
@@ -183,24 +183,129 @@ public:
             std::cout << '/' << den;
         }
     }
+
+//2zaniatie
+    //Fraction operator~()
+    //{
+    //    return Fraction(this->den, this->num, this->neg);
+    //}
+
+    //Fraction operator/(Fraction& rhs) 
+    //{
+    //    if (this->den == 0)
+    //    {
+    //        throw std::invalid_argument("Division by zero");
+    //    }
+    //    return ((*this)*(~rhs));
+    //}
+
+    Fraction& operator++() //префикс
+    {
+        return ++(*this);
+    }
+
+    Fraction& operator++(int) //постфикс
+    {
+        Fraction temp = *this;
+        ++(*this);
+        return temp;
+    }  
+    //3zaniatie
+    Fraction& operator+=(const Fraction& rhs)
+    {
+        this->num += rhs.num * den;
+        this->den = rhs.den; 
+        normalizeFraction();
+        return*this;
+    }
+
+    /*Fraction operator+(const int& lhs, const Fraction& rhs)
+    {
+        Fraction temp(*this);
+        return temp += rhs;
+    }*/
+
+    Fraction& operator=(const Fraction& rhs)
+    {
+        if (this != &rhs)
+        {
+            num = rhs.num;
+            den = rhs.den;
+        }
+        return *this;
+    }
+    //4zaniatie
+    void setNum(const uint64_t num) 
+    {
+        this->num = num;
+    }
+    void setDen(const uint64_t den) 
+    {
+        if (den == 0) 
+        {
+            throw std::invalid_argument("Division by zero");
+        }
+        this->den = den;
+    }
+    void setNeg(const bool neg)
+    {
+        this->neg = neg;
+    }
+    uint64_t getNum() const 
+    {
+        return(this->num);
+    }
+    uint64_t getDen() const 
+    {
+        return(this->den);
+    }
+    bool getNeg() const 
+    {
+        return(this->neg);
+    }
+
+    //Fractiom(int _num, int _den) : num(_num), den(_den) {}; //способ передачи параметров
+    //operator std::string() const;
+    //operator double() const;
+
+    //operator double() const
+    //{
+    //    int neg{1};
+    //    if (this->neg) 
+    //    {
+    //        neg = -1;
+    //    }
+    //    return(static_cast<double>(this->getNum()) / this->getDen() * neg);
+    //}
+
+    explicit Fraction(const double d)
+    {
+        this->num = int(d * 100);
+        this->den = 100;
+        return(this->getNum()) / this->getDen();
+    }
 };
+
 
 int main()
 {
     try
     {
-        Fraction a(4, 12);   // = -1/3
-        Fraction b(5, 6);     // =  5/6
-
-        Fraction sum = a + b;        //  1/2
-        Fraction diff = a - b;       // -7/6
-        Fraction prod = a * b;       // -5/18
-        Fraction quot = a / b;       // -2/5
+        Fraction a(-4, 12);     // -1/3
+        Fraction b(5, 6);       //  5/6
+        Fraction sum = a + b;   //  1/2
+        Fraction diff = a - b;  // -7/6
+        Fraction prod = a * b;  // -5/18
+        Fraction quot = a / b;  // -2/5
 
         a.print();  std::cout << " + ";  b.print();  std::cout << " = "; sum.print();  std::cout << '\n';
         a.print();  std::cout << " - ";  b.print();  std::cout << " = "; diff.print(); std::cout << '\n';
         a.print();  std::cout << " * ";  b.print();  std::cout << " = "; prod.print(); std::cout << '\n';
         a.print();  std::cout << " : ";  b.print();  std::cout << " = "; quot.print(); std::cout << '\n';
+
+        Fraction fraction(1, 2);
+        double d = fraction;
+        std::cout << d;
     }
     catch (const std::exception& e)
     {
